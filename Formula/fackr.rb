e005292eaf6a8203d8029bb71a881844194ad43e
espadonne <espadonne@outlook.com>
2025-12-07 21:47:34 +0000

    Add fackr formula v0.4.0

---
+++ Formula/fackr.rb
@@ -0,0 +1,18 @@
+class Fackr < Formula
+  desc "Terminal text editor written in Rust - facsimile reimplementation"
+  homepage "https://github.com/TenseleyFlow/fackr"
+  url "https://github.com/TenseleyFlow/fackr/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "872df39fba0f3ad0593b21733596185798f4fa2240747c4cade963baf725d5f4"
+  license "MIT"
+  head "https://github.com/TenseleyFlow/fackr.git", branch: "trunk"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "fackr", shell_output("#{bin}/fackr --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# Homebrew Tap for fackr
+
+This is the Homebrew tap for [fackr](https://github.com/TenseleyFlow/fackr), a terminal text editor written in Rust.
+
+## Installation
+
+```bash
+brew tap TenseleyFlow/fackr
+brew install fackr
+```
+
+## Usage
+
+```bash
+fackr [file]
+```
+
+## About
+
+fackr is a Rust reimplementation of [facsimile](https://github.com/FortranGoingOnForty/facsimile), providing a modern terminal text editor with VSCode-style keybindings.
