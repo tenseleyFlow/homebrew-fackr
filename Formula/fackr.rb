395378bed13a4da6cac07bda8934ebb77dbdb5bb
espadonne <espadonne@outlook.com>
2025-12-21 01:59:48 +0000

    Update to v1.0.3

--- Formula/fackr.rb
+++ Formula/fackr.rb
@@ -1,8 +1,8 @@
 class Fackr < Formula
   desc "Terminal text editor written in Rust - facsimile reimplementation"
   homepage "https://github.com/TenseleyFlow/fackr"
-  url "https://github.com/TenseleyFlow/fackr/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "a88cc2ee020dc8f0d2fce02deb01ad99c46403c3b69f8e502c8aac96a1c958d3"
+  url "https://github.com/TenseleyFlow/fackr/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "37c184647d4ee15ca64ffd7274dfee1976552d9cf6913e8687b6a4c84754607f"
   license "MIT"
   head "https://github.com/TenseleyFlow/fackr.git", branch: "trunk"
 
